c4493dc532311d999e1d794573d73b66432570fa
AbdullahAlomran <Abdullah.alomran@ucdconnect.ie>
2014-10-07 00:22:32 +0100

    Final version
    Abdullah Alomran
    11107715
    Assignment 1 (Skippy)

--- skippy/kangaroo.rb
+++
@@ -1,7 +0,0 @@
-require_relative'point.rb'
-
-
-class Kangaroo
-
-
-end
---
+++ skippy2/main.rb
@@ -0,0 +1,24 @@
+require_relative 'die.rb'
+require_relative 'grid.rb'
+require_relative 'kangroo.rb'
+require_relative 'point.rb'
+
+class Main
+  game=Kangaroo.new 'Kangroo game'
+  game_die=Die.new 0,0
+  game_point= Point.new 0,0
+  game.start_hopping
+
+
+  while true
+
+    game_die.throw1
+    game_point.new_point
+    game_point.reset_direction
+    game_point.print_coordinates
+    game_point.at_home?
+
+  end
+
+
+end
